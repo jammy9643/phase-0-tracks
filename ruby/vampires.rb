5015ad8c7eca864d6692f9024c9e6e8c434acbfc
Jasmine Law <ja.law.96@gmail.com>
2016-05-09 11:49:55 -0500

    Space out questionnaire, added second elsif statement (age & year = false, garlic & insurance = only one has to be true)

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,17 +1,22 @@
 puts "What is your name?"
   name = gets.chomp
+
 puts "How old are you?"
   age = gets.chomp.to_i
+
 puts "What year were you born?"
   year = gets.chomp.to_i
+
 puts "Our company cafeteria serves garlic bread. Should we order some for you (y/n)?"
   garlic = gets.chomp
+  
 puts "Would you like to enroll to the company's health insurance (y/n)?"
   insurance = gets.chomp
 
 if (age == 2016 - year) && (garlic == "yes" || insurance == "yes")
   puts "Probably not a vampire."
+elsif (age != 2016 - year) && (garlic == "no" || insurance == "no")
+  puts "Probably a vampire."
 else
   puts "Results inconclusive."
 end
-
