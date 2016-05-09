4937dfcea831f5790dcede90f25599009eefd90b
Jasmine Law <ja.law.96@gmail.com>
2016-05-09 11:34:54 -0500

    Fix bug by setting answers as integer values & created if/else statement on vampire-detection (age & year = true, garlic & insurance = only one has to be true)

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,4 +1,17 @@
 puts "What is your name?"
-puts "How old are you? What year were you born?"
-puts "Our company cafeteria serves garlic bread. Should we order some for you?"
-puts "Would you like to enroll to the company's health insurance?"
+  name = gets.chomp
+puts "How old are you?"
+  age = gets.chomp.to_i
+puts "What year were you born?"
+  year = gets.chomp.to_i
+puts "Our company cafeteria serves garlic bread. Should we order some for you (y/n)?"
+  garlic = gets.chomp
+puts "Would you like to enroll to the company's health insurance (y/n)?"
+  insurance = gets.chomp
+
+if (age == 2016 - year) && (garlic == "yes" || insurance == "yes")
+  puts "Probably not a vampire."
+else
+  puts "Results inconclusive."
+end
+
