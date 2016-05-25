6a340a26a98b2bde0319af607b6634f78eb2e41d
Jasmine Law <ja.law.96@gmail.com>
2016-05-25 16:58:00 -0500

    5.6 Release 0: swap first & last name, change all vowels to next vowels, & consonants to next consonants.

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,20 @@
+=begin
+
+Fake Name
+- ask for name
+  - save name in name variable
+    - downcase all letters
+    - change all vowels to next vowels
+    - change all consonants to next consonants
+    - split first & last name into 2 words
+      - capitalize the first letter of both words
+    - swap first & last name
+    - join split words back together with a space between
+- prints name
+
+=end
+
+puts "Name:"
+  name = gets.chomp.downcase.tr('aeiou', 'eioua').tr('bcdfghjklmnpqrstvwxyz', 'cdfghjklmnpqrstvwxyzb').split.map{|spy| spy.capitalize}.reverse.join(' ')
+
+puts name
