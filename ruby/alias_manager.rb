0c50eb177434bc0e1543943aff401e95c520c57a
Jasmine Law <ja.law.96@gmail.com>
2016-05-27 17:34:25 -0500

    5.6 Create 2 arrays to save old & changed names, turn the 2 arrays into key-value hash pairs, print sentence with both old & changed name.

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,8 +1,13 @@
 =begin
 
-Fake Name
-- ask for name
-  - save name in name variable
+Spy Name
+- create old array & new array
+- ask for name in a loop
+  - save name in old array
+  - stop loop when quit is inputted
+- change each name in old array
+  - delete the word "quit" from old array
+  - save changed name in new array
     - downcase all letters
     - change all vowels to next vowels
     - change all consonants to next consonants
@@ -10,15 +15,28 @@ Fake Name
       - capitalize the first letter of both words
     - swap first & last name
     - join split words back together with a space between
-- prints name
+- combine old array & new array into spy hash as key-value pairs
+- loop through all spy key-value pairs to print "key (old array) is actually value (new array)."
 
 =end
 
-name = ""
 
-until name == "quit" || name == "Raov"
+
+old = []
+new = []
+
+until old.include?("quit")
   puts "Name:"
-    name = gets.chomp.downcase.tr('aeiou', 'eioua').tr('bcdfghjklmnpqrstvwxyz', 'cdfghjklmnpqrstvwxyzb').split.map{|spy| spy.capitalize}.reverse.join(' ')
+    old << gets.chomp
+end
 
-  puts name
+new = old.map do |o|
+  old.delete("quit")
+  o.downcase.tr('aeiou', 'eioua').tr('bcdfghjklmnpqrstvwxyz', 'cdfghjklmnpqrstvwxyzb').split.map{|name| name.capitalize}.reverse.join(' ')
+end
+
+spy = old.zip(new).to_h
+
+spy.each do |o, n|
+  puts "#{o} is actually #{n}."
 end
