daf14f3b22336f1fd85120c06db1874fc592b1f1
Jasmine Law <ja.law.96@gmail.com>
2016-05-29 14:31:19 -0500

    Create own Hobby class with crafts, food & initialize methods.

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -32,3 +32,24 @@ puts Puppy.new.speak(3)
 puts Puppy.new.roll_over("rolls over")
 puts Puppy.new.dog_years(5)
 puts Puppy.new.nickname("Spike")
+
+
+
+class Hobby
+
+  def crafts(art)
+    puts "My hobby is #{art}!"
+  end
+
+  def food(a)
+    puts "Nom "*a.to_i
+  end
+
+  def initialize
+    puts "Initializing new Hobby instance..."
+  end
+
+end
+
+puts Hobby.new.crafts("pottery")
+puts Hobby.new.food(3)
