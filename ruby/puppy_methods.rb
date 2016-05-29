ed9335ddf66e295af7213ccdc434bf973f47e3fa
Jasmine Law <ja.law.96@gmail.com>
2016-05-29 14:17:23 -0500

    Add speak method that prints Woof an integer amount of times, add roll_method that prints rolls over, add dog_years method that computes human years to dog years, & add nickname converter that next every letter, reverse & capitalize.

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,6 +5,26 @@ class Puppy
     toy
   end
 
+  def speak(x)
+    puts "Woof "*x.to_i
+  end
+
+  def roll_over(roll)
+    puts roll
+  end
+
+  def dog_years(y)
+    puts y.to_i*7
+  end
+
+  def nick_name(blah)
+    puts blah.downcase.split('').map{|puppy| puppy.next}.reverse.join('').capitalize
+  end
+
 end
 
 puts Puppy.new.fetch("ball")
+puts Puppy.new.speak(3)
+puts Puppy.new.roll_over("rolls over")
+puts Puppy.new.dog_years(5)
+puts Puppy.new.nick_name("Spike")
