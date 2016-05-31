e48f3f9a3d37fdf95b7224efac7074bac4657587
Jasmine Law <ja.law.96@gmail.com>
2016-05-31 01:36:38 -0500

    6.3 Release 1 - Add gender, ethnicity, reindeer ranking, & age attributes to Santa class.

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,18 +1,25 @@
 class Santa
 
+  def initialize(gender, ethnicity)
+    puts "Initializing Santa instance ..."
+    @gender = gender
+    @ethnicity = ethnicity
+    @age = 0
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+  end
+
   def speak
     puts "Ho, ho, ho! Haaaappy holidays!"
+    puts "I'm the unique #{@gender} #{@ethnicity} Santa with a baby age of #{@age}."
+    puts "Now come my #{@reindeer_ranking} reindeer!"
   end
 
   def eat_milk_and_cookies(cookie)
     puts"That was a good #{cookie}!" 
   end
 
-  def initialize
-    puts "Initializing Santa instance ..."
-  end
-
 end
 
-puts Santa.new.speak
-puts Santa.new.eat_milk_and_cookies("snickerdoodle")
+santa = Santa.new("transgender", "American")
+puts santa.speak
+puts santa.eat_milk_and_cookies("snickerdoodle")
