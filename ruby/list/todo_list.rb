fb9d5aa4351fefd312c23826f1cd0629cc283ff8
Jasmine Law <ja.law.96@gmail.com>
2016-05-31 06:27:22 -0500

    6.6 Release 3 - Fix error 3 by adding delete_item method to delete item from array list attribute & return array list attribute values.

--- ruby/list/todo_list.rb
+++ ruby/list/todo_list.rb
@@ -2,11 +2,20 @@ class TodoList
   def initialize(dish, lawn)
     @list = [dish, lawn]
   end
+
   def get_items
     @list
   end
-  def add_item(item)
-    @list << item
+
+  def add_item(add)
+    @list << add
     @list
   end
+
+  def delete_item(delete)
+    @list.delete(delete)
+    @list
+  end
+
+
 end
