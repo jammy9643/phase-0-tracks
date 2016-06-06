16d1c2b0745b3aeeb2e98fff662bbe3c081add8f
Jasmine Law <ja.law.96@gmail.com>
2016-06-05 21:26:49 -0500

    5.2 - Redo Release 1.6 & add spacing to all questions.

--- ruby/arrays_drill.rb
+++ ruby/arrays_drill.rb
@@ -12,29 +12,47 @@ def add(array, item)
 end
 p add(["blue", "red", "orange"], "yellow")
 
+
+
 #Release 1
 
 #1.
 ary = Array.new
 p ary
+
 #2.
 ary.push("a","b","c","d","e")
 p ary
+
 #3. 
 ary.delete_at(2)
 p ary
+
 #4.
 ary.insert(2,"new item")
 p ary
+
 #5.
 ary.shift
 p ary
+
 #6.
-ary.include?(2) #needs work
-p ary
+def array(item)
+	ary = ["b", "new item", "d", "e"]
+	if ary.include?(item) == true
+		p "It does include it"
+	else
+		ary.include?(item) == false
+		p "It does not include it"
+	end
+end
+
+array("new item")
+
 #7.
 new_ary = [1,2,3,4,5]
 p new_ary
+
 #8.
 two_arys = ary + new_ary
 p two_arys
